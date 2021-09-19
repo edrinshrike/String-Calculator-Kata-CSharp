1a5472e9e47c0fb56fab68d6ffb485b9fb90d3ed
Steven Matthews <smatthews@kalamazooxray.com>
2021-09-19 10:22:26 -0400

    Add project files.

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Calculate(string numbers)
+        {
+            if(string.IsNullOrEmpty(numbers)) { return 0; }
+
+            char operand = '+';
+
+            List<char> possibleOperands = new List<char> { '+', '-', '*', '/' };
+
+            if (possibleOperands.Contains(numbers.Last()))
+            {
+                operand = numbers.Last();
+                numbers = numbers.Remove(numbers.Length - 2, 2);
+            }
+
+            var numArray = numbers.Split(',').Select(Int32.Parse).ToList();
+
+            if (CheckForNegatives(numArray))
+            {
+                throw new InvalidOperationException("negatives not allowed");
+            }
+
+            int result = numArray[0];
+
+            for(int i = 1; i < numArray.Count();  i++)
+            {
+
+                switch (operand)
+                {
+                    case '+':
+                        result += numArray[i];
+                        break;
+
+                    case '-':
+                        result -= numArray[i];
+                        break;
+
+                    case '*':
+                        result *= numArray[i];
+                        break;
+
+                    case '/':
+                        result /= numArray[i];
+                        break;
+                }
+            }
+
+            return result;
+        }
+
+        bool CheckForNegatives(List<int> numbers)
+        {
+            bool hasNegatives = false;
+
+            foreach(int i in numbers)
+            {
+                if (i < 0)
+                {
+                    hasNegatives = true;
+                }
+            }
+
+            return hasNegatives;
+        }
+    }
+}
---
+++ StringCalculatorTests/CalculatorNUnitTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    class CalculatorNUnitTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new();
+        }
+
+        [Test]
+        public void Add_InputEmptyString_ReturnZero()
+        {
+            int result = calculator.Calculate("");
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_InputSingleDigitOf5_Return5()
+        {
+            int result = calculator.Calculate("5");
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Add_InputFourAndTwo_ReturnSix()
+        {
+            int result = calculator.Calculate("4,2");
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Add_InputFourTwoAndOne_ReturnSeven()
+        {
+            int result = calculator.Calculate("4,2,1");
+
+            Assert.That(result, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void Add_InputNegativeNumber_GetException()
+        {
+            var result = Assert.Throws<InvalidOperationException>(() => calculator.Calculate("5,-2"));
+
+            Assert.That(result.Message, Is.EqualTo("negatives not allowed"));
+        }
+
+        [Test]
+        public void Add_InputAdditionOperand_ReturnNumbersAdded()
+        {
+            int result = calculator.Calculate("4,2,+");
+
+            Assert.That(result, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Add_InputSubtractionOperand_ReturnNumbersSubtracted()
+        {
+            int result = calculator.Calculate("4,2,-");
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Add_InputMultiplicationOperand_ReturnNumbersMultiplied()
+        {
+            int result = calculator.Calculate("4,2,*");
+
+            Assert.That(result, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void Add_InputDivisionOperand_ReturnNumbersDivided()
+        {
+            int result = calculator.Calculate("4,2,/");
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
